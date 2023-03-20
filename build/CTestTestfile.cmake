1b0cb2d7afd55bf674aafcc6c68e3a5ad5beca7c
anh <DucAnh2808>
2023-03-20 15:33:31 +0700

    a

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/anh/ws_2/src
+# Build directory: /home/anh/ws_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("smb_common_v2/smb_control")
+subdirs("smb_common_v2/smb_gazebo")
+subdirs("smb_common_v2/smb_description")
+subdirs("smb_highlevel_controller")
